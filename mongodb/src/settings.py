860fd5c7109620fa626d0322ea2554132a7959b8
HughDEV <hughpythoneer@gmail.com>
2025-05-26 18:55:50 -0400

    updated README.md

--- mongodb/src/settings.py
+++ mongodb/src/settings.py
@@ -2,3 +2,10 @@ import sys
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the MongoDB connection string from environment variables
+MONGODB_URI = os.getenv("MONGODB_URI")
+print(f"MongoDB connection string: {MONGODB_URI}")
